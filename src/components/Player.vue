6ee80fe2cfc8c1ffada4bea22714856cbcd8c331
youzheJ <946008612@qq.com>
2017-10-19 23:48:38 +0800

    将线条颜色改为渐变色

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -37,36 +37,38 @@
         canvas.width = width
         canvas.height = height
         const ctx = canvas.getContext('2d')
-        ctx.fillStyle = '#000' // 背景的颜色
+        ctx.fillStyle = '#fff' // 背景的颜色
         ctx.fillRect(0, 0, width, height) // 初始化背景
-        const curve1 = new Curve(ctx, width - 50, height, getColor(), 120, w / 2, 300)
-        const curve2 = new Curve(ctx, width - 200, height, getColor(), 30, w, 300)
-        const curve3 = new Curve(ctx, width - 120, height, getColor(), 50, w / 3, 300)
-        const curve4 = new Curve(ctx, width - 140, height, getColor(), 50, w * 0.8, 320)
-        const curve5 = new Curve(ctx, width - 130, height, getColor(), 50, w * 1.2, 290)
+        const curve1 = new Curve(ctx, width * 0.96, height, 0, height / 7, w / 2, height * 0.43)
+        const curve2 = new Curve(ctx, width * 0.90, height, 1, height / 20, w, height * 0.43)
+        const curve3 = new Curve(ctx, width * 0.80, height, 2, height / 5, w * 1.05, height * 0.43)
+        const curve4 = new Curve(ctx, width * 0.95, height, 3, height / 18, w * 0.8, height * 0.46)
+        const curve5 = new Curve(ctx, width * 0.93, height, 4, height / 13, w * 1.2, height * 0.41)
+        const curve6 = new Curve(ctx, width * 0.98, height, 5, height / 20, w * 1.6, height * 0.46, 1)
+        const curve7 = new Curve(ctx, width / 2, height, 3, 100, w * 2.2, 280, 1)
         setInterval(() => { // 开始绘制
-          drawCanvas(ctx, [curve1, curve2, curve3, curve4, curve5], width, height, step, LENGTH, xOffset)
+          drawCanvas(ctx, [curve1, curve2, curve3, curve4, curve5, curve6, curve7], width, height, step, LENGTH, xOffset)
           xOffset += SPEED
         }, 16)
       }
       const drawCanvas = (ctx, curves, width, height, step, LENGTH, xOffset) => { // 绘制内容
-        ctx.clearRect(0, 0, width, height)
         ctx.fillRect(0, 0, width, height) // 绘制背景
         curves.map((curve, i) => {
           curve.drawCurve(step, LENGTH, xOffset)
         })
       }
-      function getNum (max, min) { // 生成包含min 和 max的数
-        return Math.round(Math.random() * (max - min) + min)
-      }
-      function getColor () { // 生成颜色
-        let c0 = getNum(10, 45)
-        let c1 = getNum(180, 240)
-        let p = Math.random()
-        return `rgb(${p < 0.5 ? c1 : c0}, ${p < 0.5 ? c1 : c0}, ${p < 0.5 ? c0 : c1})`
+      // function getNum (max, min) { // 生成包含min 和 max的数
+      //   return Math.round(Math.random() * (max - min) + min)
+      // }
+      function getSoftColor (x = 0) { // x: 0 - 2 * Math.PI
+        let c = Math.cos
+        let t = Math.PI * 2
+        let r = Math.round
+        // x = limitNum(x) // 过滤部分颜色
+        return `rgb(${r(c(x) * 127 + 128)}, ${r(c(x + t / 3) * 127 + 128)}, ${r(c(x + t / 3 * 2) * 127 + 128)})`
       }
       class Curve {
-        constructor (ctx, width, height, color, h, w, b) {
+        constructor (ctx, width, height, color, h, w, b, lineWidth = 2) {
           this.ctx = ctx // 背景canvas
           this.width = width // 横标的总长
           this.height = height // 纵坐标的总长
@@ -74,6 +76,7 @@
           this.h = h // 决定曲线高度
           this.w = w // 决定波长
           this.b = b // 上下偏移量
+          this.lineWidth = lineWidth // 线宽
           this.canVirtual = document.createElement('canvas')
           this.canVirtual.width = width
           this.canVirtual.height = height
@@ -91,9 +94,17 @@
             y = this.sin(x, xOffset)
             this.ctxVir.lineTo(x, y)
           }
-          // this.ctxVir.strokeStyle = '#06e9a3'
-          this.ctxVir.strokeStyle = this.color
-          this.ctxVir.lineWidth = 3
+          this.ctxVir.shadowBlur = 5 // 设置阴影模糊度
+          this.ctxVir.shadowColor = 'rgba(155,155,155,0.3)' // 设置阴影颜色
+          var gradient = this.ctxVir.createLinearGradient(0, 0, this.width, this.height) // x0,y0,x1,y1
+          gradient.addColorStop(0, 'white')
+          gradient.addColorStop(0.1, 'white')
+          gradient.addColorStop(0.3, getSoftColor(xOffset / 5 + this.color + Math.PI / 3 * 2))
+          gradient.addColorStop(0.6, getSoftColor(xOffset / 5 + this.color + Math.PI / 3))
+          gradient.addColorStop(0.8, getSoftColor(xOffset / 5 + this.color + Math.PI / 6))
+          gradient.addColorStop(1, getSoftColor(xOffset / 5 + this.color))
+          this.ctxVir.strokeStyle = gradient
+          this.ctxVir.lineWidth = this.lineWidth
           this.ctxVir.stroke()
           this.ctx.drawImage(this.canVirtual, 0, 0)
         }
